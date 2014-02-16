4521afd8e12a44020ca39a0835a0279c7b582f6b
PamExx <thomas@pamexx.com>
2014-02-16 00:41:33 -0500

    Added code to print the time to the cleaned data file

--- clean_bitstamp_data.ml
+++ clean_bitstamp_data.ml
@@ -8,7 +8,7 @@ let out_file = "/home/alpha/Bitcoin Data/BTC_Bitstamp_clean.csv" in
 
 (* Original file headers for all columns were:
  * Ticker, Per, Date, Time, Open, High, Low, Close, Volume *)
-let headers = "Date,Open,High,Low,Close,Volume\n" in
+let headers = "Date,Time,Open,High,Low,Close,Volume\n" in
 
 (* Open the in_channel *)
 let (ic : in_channel) = open_in in_file in
@@ -26,12 +26,30 @@ let write_line (sl : string list) =
     let year = String.sub date 0 4 in
     let month = String.sub date 4 2 in
     let day = String.sub date 6 2 in
+    let time = List.nth sl 3 in
+
+    let hour = 
+      if String.length time = 6 then String.sub time 0 2
+      else if String.length time = 5 then "0"^(String.sub time 0 1)
+      else "00" 
+    in
+
+    let minute = 
+      if String.length time = 6 then String.sub time 2 2
+      else if String.length time = 5 then String.sub time 1 2 
+      else if String.length time = 4 then String.sub time 0 2
+      else if String.length time = 3 then "0"^(String.sub time 0 1)
+      else "00"
+    in
+
     let open_price = List.nth sl 4 in
     let high_price = List.nth sl 5 in
     let low_price = List.nth sl 6 in
     let close_price = List.nth sl 7 in
     let volume = List.nth sl 8 in 
+
     (year^"-"^month^"-"^day^","^
+     hour^":"^minute^","^
      open_price^","^
      high_price^","^
      low_price^","^
@@ -52,48 +70,3 @@ Csv.iter write_line csv_ic;
 
 (* Close the CSV in_channel -- the underlying channel is close as well *)
 Csv.close_in csv_ic;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(*
-
-let _ = 
-  let open Core_kernel.In_channel in
-  let ic : in_channel = create test_file in                (* Create an in_channel for the file *)
-  
-  let print_to_new_file s:string = 
-    let open Core_kernel in
-    let string_list = Core_string.split s ~on: ',' in  (* Split the string into a list of words *)
-    let raw_date = List.nth string_list 3 in
-    (*let year = Core_string.slice raw_date 0 4 in
-    let month = Core_string.slice raw_date 4 6 in
-    let day = Core_string.slice raw_date 6 8 in*)
-
-    (*let string_to_print = Core_string.concat ~sep:"," [year; month; day] in*)
-
-    print_string (raw_date^"\n");
-    (*print_string (year^"\n");
-    print_string (month^"\n");
-    print_string (day^"\n");*)
-
-  in
-    iter_lines ic print_to_new_file;
-    close ic;
-
-    *)
